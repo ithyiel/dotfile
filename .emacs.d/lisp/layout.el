2058ec7efaa3d81232ba313ecbd0aef435bc83fd
ithyiel <ithyiel@gmail.com>
2019-04-29 17:17:32 +0800

    ,

--- .emacs.d/lisp/layout.el
+++ .emacs.d/lisp/layout.el
@@ -1,6 +1,15 @@
 ;; layout
 
+(global-set-key [?\C-o] 'find-file)
 (global-set-key [?\C-,] 'buffer-menu)
 (global-set-key [?\C-\;] 'kill-buffer)
+(global-set-key [?\C-'] 'delete-window)
+
+(key-chord-define-global [?,?d] 'other-window)
+(key-chord-define-global [?/?d] 'split-window-right)
+(key-chord-define-global [?/?s] 'split-window-below)
+
+(key-chord-define-global [?.?a] 'previous-buffer)
+(key-chord-define-global [?.?d] 'next-buffer)
 
 (provide 'layout)
