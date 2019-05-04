abcde2acb912deaff8e0e06969323734905163e6
ithyiel <ithyiel@gmail.com>
2019-05-04 12:04:23 +0800

    ,

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,12 +4,12 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+(require 'packag)
 (progn (require 'key-chord) (key-chord-mode t))
 (require 'layout)
 (require 'launch)
 
 (setq custom-file (expand-file-name "prefs.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(when (file-exists-p custom-file) (load custom-file))
 
 (provide 'init)
---
+++ .emacs.d/lisp/packag.el
@@ -0,0 +1,5 @@
+;; package
+
+(package-initialize)
+
+(provide 'packag)
