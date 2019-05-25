b6dc8cef23bb177c8d1a5ac88650caf3f1225a7f
ithyiel <ithyiel@gmail.com>
2019-05-25 16:10:20 +0800

    ,

--- .emacs.d/lisp/maps.el
+++ .emacs.d/lisp/maps.el
@@ -8,18 +8,43 @@
   (and (listp map) (listp (cdr map))
        (and (symbolp type) (eq type (car map)))))
 
-(defun map-get-map (map type key &optional testfn)
+(defun map-get (map type key &optional testfn)
   (let (return)
     (and (mapp/ map type)
 	 (setq return
 	       (let ((tail (cdr map)))
 		 (catch 'done
 		   (while (consp tail)
-		     ;; alist map
 		     (and (consp (car tail))
 			  (let* ((this-map (car tail)) (this-key (car this-map)))
 			    (if (funcall (or testfn 'eq) this-key key) (throw 'done this-map))))
 		     (setq tail (cdr tail)))))))
+    return))
+
+(defun map-get-before (map type key &optional testfn)
+  (let (return)
+    (and (mapp/ map type)
+	 (let ((tail (cdr map)))
+	   (catch 'break
+	     (while (consp tail)
+	       (and (consp (car tail))
+		    (let* ((this-map (car tail)) (this-key (car this-map)))
+		      (if (funcall (or testfn 'eq) this-key key)
+			  (throw 'break nil)
+			(setq return (append return (list this-map))))))
+	       (setq tail (cdr tail))))))
+      return))
+
+(defun map-get-after (map type key &optional testfn)
+  (let (return)
+    (and (mapp/ map type)
+	 (let ((tail (cdr map)) found)
+	   (while (consp tail)
+	     (and (consp (car tail))
+		  (let* ((this-map (car tail)) (this-key (car this-map)))
+		    (if found (setq return (append return (list this-map))))
+		    (if (funcall (or testfn 'eq) this-key key) (setq found t))))
+	     (setq tail (cdr tail)))))
     return))
 
 (defun map-map (map type key bind)
@@ -27,7 +52,7 @@
        (append map (list
 		    (let* ((this-key (elt key 0))
 			   (rest-key (seq-drop key 1))
-			   (this-map (map-get-map map type this-key)))
+			   (this-map (map-get map type this-key)))
 		      (if (null this-map)
 			  (cons this-key
 				(if (> (length rest-key) 0)
